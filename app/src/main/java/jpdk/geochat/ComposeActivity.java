939a5b3581d8fdd1eed672477f34786f7d160b20
Peter Li <tyuo9980@gmail.com>
2015-01-31 12:25:05 -0500

    Morning of day 2, 24 hrs remain

--- app/src/main/java/jpdk/geochat/ComposeActivity.java
+++ app/src/main/java/jpdk/geochat/ComposeActivity.java
@@ -1,17 +1,52 @@
 package jpdk.geochat;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class ComposeActivity extends ActionBarActivity {
+public class ComposeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+
         setContentView(R.layout.activity_compose);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(), "Message Posted!", Toast.LENGTH_SHORT);
+                System.out.println("Message Posted!");
+            }
+        });
+
+        final TextView charLeft = (TextView) findViewById(R.id.charLeftText);
+        final EditText messageArea = (EditText) findViewById(R.id.messageArea);
+
+        messageArea.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                charLeft.setHint((140 - messageArea.getText().length()) + " characters left");
+            }
+        });
     }
 
 
--- app/src/main/java/jpdk/geochat/LoginActivity.java
+++ app/src/main/java/jpdk/geochat/LoginActivity.java
@@ -1,39 +1,23 @@
 package jpdk.geochat;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
 
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends FragmentActivity {
+
+    private LoginFragment loginFragment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (savedInstanceState == null) {
+            loginFragment = new LoginFragment();
+            getSupportFragmentManager().beginTransaction().add(android.R.id.content, loginFragment).commit();
+        } else {
+            loginFragment = (LoginFragment) getSupportFragmentManager().findFragmentById(android.R.id.content);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
